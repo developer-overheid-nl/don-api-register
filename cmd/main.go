e4be9412f06176c80f2286dcaa84a857909d5bb7
pasibun <matthijs_hovestad@hotmail.com>
2025-04-03 11:08:44 +0200

    logging

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,9 @@ func main() {
 	APIsAPIController := api.NewAPIsAPIController(APIsAPIService)
 
 	router := api.NewRouter(APIsAPIController)
-
+	log.Println("Server is running on port 8080")
+	log.Println("http://localhost:8080/apis/v1/apis")
+	log.Println("http://localhost:8080/apis/v1/apis/1")
 	log.Fatal(http.ListenAndServe(":8080", router))
+
 }

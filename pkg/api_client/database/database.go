c011baed3eacf988f579d1aab12f97fa76355fdd
Dimitri van Hees <info@dimitrivanhees.com>
2025-08-20 13:34:11 +0200

    test new tables

--- pkg/api_client/database/database.go
+++ pkg/api_client/database/database.go
@@ -2,14 +2,19 @@ package database
 
 import (
 	"fmt"
+
 	"github.com/developer-overheid-nl/don-api-register/pkg/api_client/models"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func Connect(connStr string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix: "v1_", // add your prefix here
+		}})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
